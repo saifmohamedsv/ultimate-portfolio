c46ee2290a8f86664ead69b483cff81a3ef4bedb
Saif Mohamed <saifmohamed.dev@gmail.com>
2025-11-09 22:41:23 +0200

    feat(ui): refine floating dock animations and interactivity

--- components/ui/floating-dock.tsx
+++ components/ui/floating-dock.tsx
@@ -32,7 +32,7 @@ export const FloatingDock = ({
   return (
     <>
       <FloatingDockDesktop items={items} className={desktopClassName} />
-      <FloatingDockMobile items={items} className={mobileClassName} />
+      {/* <FloatingDockMobile items={items} className={mobileClassName} /> */}
     </>
   );
 };
@@ -136,19 +136,19 @@ const FloatingDockDesktop = ({
     <motion.div
       onMouseMove={(e) => mouseX.set(e.pageX)}
       onMouseLeave={() => mouseX.set(Infinity)}
-      animate={{
-        backdropFilter: visible ? "blur(10px)" : "none",
-        boxShadow: visible
-          ? "0 0 24px rgba(34, 42, 53, 0.06), 0 1px 1px rgba(0, 0, 0, 0.05), 0 0 0 1px rgba(34, 42, 53, 0.04), 0 0 4px rgba(34, 42, 53, 0.08), 0 16px 68px rgba(47, 48, 55, 0.05), 0 1px 0 rgba(255, 255, 255, 0.1) inset"
-          : "none",
-      }}
+      // animate={{
+      //   backdropFilter: visible ? "blur(10px)" : "none",
+      //   boxShadow: visible
+      //     ? "0 0 24px rgba(34, 42, 53, 0.06), 0 1px 1px rgba(0, 0, 0, 0.05), 0 0 0 1px rgba(34, 42, 53, 0.04), 0 0 4px rgba(34, 42, 53, 0.08), 0 16px 68px rgba(47, 48, 55, 0.05), 0 1px 0 rgba(255, 255, 255, 0.1) inset"
+      //     : "none",
+      // }}
       transition={{
         type: "spring",
         stiffness: 200,
         damping: 50,
       }}
       className={cn(
-        "mx-auto h-16 items-end w-fit gap-4 rounded-2xl px-4 py-3 hidden md:flex bg-transparent",
+        "mx-auto h-16 border items-end w-fit gap-2 md:gap-4 rounded-2xl px-2 md:px-4 py-3 flex bg-transparent",
         visible && "bg-white/80 dark:bg-neutral-950/80",
         !visible && "bg-gray-50 dark:bg-neutral-900",
         className
